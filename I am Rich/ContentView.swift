fc7a7f77de9e04a2cf49f6cd1b79f938ce0c32cc
Nico Cobelo <ingcobelo@gmail.com>
2020-11-13 15:58:56 -0500

    Built a simple iOS app using SwiftUI

--- I am Rich/ContentView.swift
+++ I am Rich/ContentView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone Xʀ"))
     }
 }
